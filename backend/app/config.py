0d78890dc39b0c00252563de596aaa303357e635
Saumya028 <saumyanshah2006@gmail.com>
2026-07-04 11:32:13 +0530

    Deploy V1

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-print("DATABASE_URL =", DATABASE_URL)
+
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
